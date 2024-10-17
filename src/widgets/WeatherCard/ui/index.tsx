2d24f52b3634ed2dd312c28225156d28a0b212cf
Muradil <muradil.koychubekob@gmail.com>
2024-10-17 13:00:44 +0600

    change design and add outfit font

--- src/widgets/WeatherCard/ui/index.tsx
+++ src/widgets/WeatherCard/ui/index.tsx
@@ -3,34 +3,50 @@ import { ForecastData } from '../../../@types/weather';
 
 interface Props {
   title: string;
+  modeDay: string;
   weatherData: ForecastData[];
 }
 
-export const WeatherCard: React.FC<Props> = ({ title, weatherData }) => {
+export const WeatherCard: React.FC<Props> = ({
+  title,
+  weatherData,
+  modeDay,
+}) => {
   if (weatherData.length === 0) return null;
 
   return (
-    <div className="d-flex justify-content-between align-items-center border-1 border-black border px-3 flex-wrap">
-      <div className="flex-grow-1">
-        <h3>{title}</h3>
-      </div>
-      <div className="flex-grow-1">
-        <img
-          src={`https://openweathermap.org/img/wn/${weatherData[0].weather[0].icon}@2x.png`}
-          alt={title}
-        />
-      </div>
-      <div>
-        {weatherData.map((item) => (
-          <div className="d-flex gap-3 flex-wrap" key={item.dt_txt}>
-            <p>Описание: {item.weather[0].main}</p>
-            <p>Температура: {item.main.temp}°C</p>
-            <p>Ощущается: {item.main.feels_like}°C</p>
-            <p>Давление: {item.main.pressure} мм</p>
-            <p>Влажность: {item.main.humidity} %</p>
+    <>
+      {weatherData.map((item) => (
+        <div
+          className={`d-flex align-items-center shadow p-3 mb-5 text-white justify-content-between px-3 w-100 ${modeDay === 'day' ? 'blue-day' : 'sky-night'}`}
+        >
+          <div className="d-flex gap-5 align-items-center">
+            <img
+              src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+              alt={title}
+            />
+            <div className="d-flex flex-column gap-2">
+              <p className="m-0">
+                {item.main.temp}°C {title}
+              </p>
+              <p className="m-0">{item.weather[0].main}</p>
+            </div>
           </div>
-        ))}
-      </div>
-    </div>
+          <div className="d-flex flex-column gap-2">
+            <h4 className="m-0">
+              {item.main.temp_max}/{item.main.temp_min}
+            </h4>
+            <div className="d-flex gap-3">
+              <img
+                style={{ width: '25px' }}
+                src="https://cdn-icons-png.flaticon.com/512/2204/2204338.png"
+                alt="humidity"
+              />
+              <span>{item.main.humidity} mm</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
